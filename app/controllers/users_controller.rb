431b6ad9225b61afabf498f82cc553a6edf1faec
cristobal-2207 <cristobalgutierreznavarro@gmail.com>
2020-11-18 05:01:00 -0300

    Se logra aplicar que, un usuario pueda liberar libros que haya reservado.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_book, only: :release
+  
   def profile
     @books=Book.where(status: :reserved)
   end
+
+  def release
+    @book.update_attributes(status: :available)
+    redirect_to profile_users_path
+  end
+
+  def set_book
+    @book=Book.find(params[:book_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :books
   root 'users#profile'
-  get 'users/profile'
+  resources :users, only: [] do
+    collection do
+      get :profile
+      put '/:book_id/release', to: 'users#release', as: 'release'
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
